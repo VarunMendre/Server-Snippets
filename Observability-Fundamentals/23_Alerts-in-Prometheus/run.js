5c5e5e034d3b69d3a6dfeac433fbd6473cf97002
Varun Mendre <varunmm0404@gmail.com>
2026-04-27 15:00:02 +0530

    feat & learn: different states in prometheus alerts & things like labels, for, annotations

--- Observability-Fundamentals/23_Alerts-in-Prometheus/run.js
+++ Observability-Fundamentals/23_Alerts-in-Prometheus/run.js
@@ -1,10 +1,12 @@
-// for (let i = 1; i <= 40; i++) fetch("http://localhost:4000/");
-// for (let i = 1; i <= 10; i++) fetch("http://localhost:4000/fail");
+for (let i = 1; i <= 40; i++) fetch("http://localhost:4000/");
+for (let i = 1; i <= 10; i++) fetch("http://localhost:4000/fail");
 // for (let i = 1; i <= 20; i++) fetch("http://localhost:4000/unstable");
-for (let i = 1; i <= 5; i++) fetch("http://localhost:4000/system-cpu");
+// for (let i = 1; i <= 5; i++) fetch("http://localhost:4000/system-cpu");
 
 setInterval(() => {
+  for (let i = 1; i <= 40; i++) fetch("http://localhost:4000/");
+for (let i = 1; i <= 10; i++) fetch("http://localhost:4000/fail");
 //   for (let i = 1; i <= 200; i++) fetch("http://localhost:4000/");
 //   for (let i = 1; i <= 20; i++) fetch("http://localhost:4000/unstable");
-  for (let i = 1; i <= 5; i++) fetch("http://localhost:4000/system-cpu");
-}, 5 * 1000);
+  // for (let i = 1; i <= 5; i++) fetch("http://localhost:4000/system-cpu");
+}, 10 * 1000);
