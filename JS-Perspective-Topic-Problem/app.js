37cfd31adc660aa606178a963f473fe351e4c4fe
Varun Mendre <varunmm0404@gmail.com>
2026-08-05 16:50:49 +0530

    chore: practicing JS questions

--- JS-Perspective-Topic-Problem/app.js
+++ JS-Perspective-Topic-Problem/app.js
@@ -115,7 +115,6 @@ function aggregate(arr) {
 const aggregateAnswer = aggregate(orders);
 // console.log(aggregateAnswer);
 
-
 // 8.
 
 const employees = [
@@ -139,6 +138,284 @@ function highestPaidSalary(arr) {
 const highest = highestPaidSalary(employees);
 // console.log(highest);
 
+// 9.
+
+const users1 = [
+  { name: "John", age: 30 },
+  { name: "Alice", age: 22 },
+  { name: "Bob", age: 27 },
+];
+
+function sortByAgeUsingBuiltIn(arr) {
+  return arr.sort((a, b) => a.age - b.age);
+}
+
+function sortByAgeWithoutBuiltIn(arr) {
+  const result = [...arr];
+
+  let n = result.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i; i++) {
+      if (result[j].age > result[j + 1].age) {
+        let temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
+
+  return result;
+}
+
+const output1 = sortByAgeUsingBuiltIn(users1);
+// console.log(output1);
+
+const output2 = sortByAgeWithoutBuiltIn(users1);
+// console.log(output2);
+
+// 10.
+const nums3 = [1, 2, 3, 4, 6];
+
+// input will always from 1 to N
+
+function findMissingNumber(arr) {
+  let n = arr.length;
+  let xor1 = 0;
+  let xor2 = 0;
+
+  for (let num of arr) {
+    xor1 ^= num;
+  }
+
+  for (let i = 1; i <= n + 1; i++) {
+    xor2 ^= i;
+  }
+
+  return xor1 ^ xor2;
+}
+
+const result1 = findMissingNumber(nums3);
+// console.log(result1);
+
+// 11.
+
+const nums4 = [1, [2, [3, 4]], 5];
+
+function flattenArray(arr) {
+  let result = [];
+
+  for (let num of arr) {
+    if (Array.isArray(num)) {
+      result.push(...flattenArray(num));
+    } else {
+      result.push(num);
+    }
+  }
+
+  return result;
+}
+
+const flatArray = flattenArray(nums4);
+// console.log(flatArray);
+
+// 12.
+
+const sentence = "node js node express js node";
+
+function countOccurrences(str) {
+  const words = str.split(" ");
+
+  let result = {};
+
+  for (let w of words) {
+    if (!result[w]) {
+      result[w] = 0;
+    }
+
+    result[w]++;
+  }
+
+  return result;
+}
+
+const obj = countOccurrences(sentence);
+// console.log(obj);
+
+// 13. Find Intersection of Two Arrays
+
+const array1 = [1, 2, 3, 4];
+const array2 = [3, 4, 5, 6];
+
+/*
+Expected Output: [3, 4]
+*/
+
+function findIntersection(arr1, arr2) {
+  let n = arr1.length;
+  let m = arr2.length;
+
+  let result = [];
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (arr1[i] == arr2[j]) {
+        result.push(arr1[i]);
+      }
+    }
+  }
+
+  return result;
+}
+
+const intersect = findIntersection(array1, array2);
+// console.log(intersect);
+
+// 14. Merge Users with Their Orders
+
+// I/P :
+
+const user = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Alice" },
+];
+
+const order = [
+  { userId: 1, product: "Laptop" },
+  { userId: 1, product: "Mouse" },
+  { userId: 2, product: "Keyboard" },
+];
+
+/* 
+output : 
+[
+    {
+        id: 1,
+        name: "John",
+        orders: ["Laptop", "Mouse"]
+    },
+    {
+        id: 2,
+        name: "Alice",
+        orders: ["Keyboard"]
+    }
+]
+*/
+
+function mergeUsers(userArray, orderArray) {
+  let result = {};
+
+  for (let item of userArray) {
+    result[item.id] = {
+      id: item.id,
+      name: item.name,
+      orders: [],
+    };
+  }
+
+  for (let order of orderArray) {
+    result[order.userId].orders.push(order.product);
+  }
+
+  return Object.values(result);
+}
+
+const userDetails = mergeUsers(user, order);
+// console.log(userDetails);
+
+// 15.
+
+const nums5 = [10, 5];
+
+/*
+I. 
+num = 10 , num > highest(-Infinity) (yes) => secondHighest = -Infinity , highest = 10;
+
+II.
+num = 5, num > highest(10) (No) else num > secondHighest && num != highest (yes) so secondHighest = 5
+
+*/
+
+function secondHighestNumber(arr) {
+  let n = arr.length;
+
+  let highest = -Infinity;
+  let secondHighest = -Infinity;
+
+  for (let num of arr) {
+    if (num > highest) {
+      secondHighest = highest;
+      highest = num;
+    } else if (num > secondHighest && num !== highest) {
+      secondHighest = num;
+    }
+  }
+  return secondHighest;
+}
+
+const highestNumber = secondHighestNumber(nums5);
+// console.log(highestNumber);
+
+// 16.
+
+const products = [
+  { name: "iPhone", category: "Mobile" },
+  { name: "Samsung", category: "Mobile" },
+  { name: "MacBook", category: "Laptop" },
+];
+
+function groupByCategory(arr) {
+  let result = arr.reduce((acc, item) => {
+    if (!acc[item.category]) {
+      acc[item.category] = [];
+    }
+
+    acc[item.category].push({ name: item.name, category: item.category });
+    return acc;
+  }, {});
+
+  return result;
+}
+
+const categories = groupByCategory(products);
+// console.log(categories);
+
+// 17. Find Users Who Never Ordered
+
+const users2 = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Alice" },
+  { id: 3, name: "Bob" },
+];
+
+const orders2 = [{ userId: 1 }, { userId: 3 }];
+
+function userWhoNeverOrder(users, orders) {
+  // const obj = {};
+
+  // for (const user of users) {
+  //   obj[user.id] = 0;
+  // }
+
+  // for (const order of orders) {
+  //   obj[order.userId]++;
+  // }
+
+  // return users.filter((user) => obj[user.id] == 0);
+
+  //2nd approach
+
+  const orderedUsers = new Set();
+
+  for (let order of orders) {
+    orderedUsers.add(order.userId);
+  }
+
+  return users.filter(user => !orderedUsers.has(user.id));
+}
+
+const user3 = userWhoNeverOrder(users2, orders2);
+console.log(user3);
 
 
-// 9. 
+
